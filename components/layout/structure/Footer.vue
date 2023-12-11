647fa4292f720272472314324b31da9482032205
jaopredo <joao.profissional.45@gmail.com>
2023-12-11 08:31:09 -0300

    Finalizações

--- components/layout/structure/Footer.vue
+++ components/layout/structure/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-[url('/prevsol-map-changed.png')] bg-no-repeat bg-cover bg-center relative text-white">
         <div class="h-5 w-full absolute top-0 left-0 bg-emerald-900"></div>
-        <div class="flex items-center justify-around pr-4 pl-4 pb-32 pt-10 z-10">
+        <div class="flex items-center justify-around flex-col md:flex-row gap-5 md:gap-0 pr-4 pl-4 pb-32 pt-10 z-10">
             <img class="w-auto h-44" src="/prevsol-2-logo.png">
             <div class="bg-gray-100 text-emerald-900 p-3 rounded-md flex flex-col items-center justify-center gap-5">
                 <div class="flex justify-center flex-col w-full">
@@ -32,12 +32,9 @@
                 </div>                    
             </div>
         </div>
-        <div class="bg-emerald-900 border-t border-t-white flex items-center justify-between p-4 z-10">
+        <div class="bg-emerald-900 border-t border-t-white flex items-center justify-between p-4 z-10 flex-col md:flex-row">
             <h1>Copyright © 2023 PREVSOL. Todos os direitos reservados.</h1>
             <ul class="flex items-center justify-center gap-4">
-                <li title="Facebook">
-                    <Icon class="hover:scale-110 transition-transform duration-300" name="fa:facebook-square" size="18.5" color="white"></Icon>
-                </li>
                 <li title="Instagram"><a href="https://www.instagram.com/prevsol" target="_blank">
                     <Icon class="hover:scale-110 transition-transform duration-300" name="fa:instagram" size="18.5" color="white"></Icon>
                 </a></li>
